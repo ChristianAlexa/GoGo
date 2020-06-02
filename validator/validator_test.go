a1728dc3554fb94f544690402f21eef664c7be9a
ChristianAlexa <cjalexa@gmail.com>
2020-06-02 00:31:05 -0400

    Adds unit tests for IsEnemyStone and IsFriendlyStone

--- validator/validator_test.go
+++ validator/validator_test.go
@@ -67,3 +67,129 @@ func TestIsEmptyIntersection(t *testing.T) {
 	})
 
 }
+
+func TestIsEnemyStone(t *testing.T) {
+
+	t.Run("A black stone and a white stone are enemies", func(t *testing.T) {
+
+		mockBlackStone := models.Stone{
+			Color:        "black",
+			LibertyCount: 4,
+		}
+
+		mockWhiteStone := models.Stone{
+			Color:        "white",
+			LibertyCount: 4,
+		}
+
+		actual := isEnemyStone(mockBlackStone.Color, mockWhiteStone.Color)
+		expected := true
+
+		if actual != expected {
+			t.Errorf("got %v and expected %v", actual, expected)
+		}
+	})
+
+	t.Run("Two white stones are not enemies", func(t *testing.T) {
+
+		mockWhiteStone1 := models.Stone{
+			Color:        "white",
+			LibertyCount: 4,
+		}
+
+		mockWhiteStone2 := models.Stone{
+			Color:        "white",
+			LibertyCount: 4,
+		}
+
+		actual := isEnemyStone(mockWhiteStone1.Color, mockWhiteStone2.Color)
+		expected := false
+
+		if actual != expected {
+			t.Errorf("got %v and expected %v", actual, expected)
+		}
+	})
+
+	t.Run("If one stone color is empty, there is no enemy", func(t *testing.T) {
+
+		mockEmptyStone := models.Stone{
+			Color:        "empty",
+			LibertyCount: 4,
+		}
+
+		mockWhiteStone := models.Stone{
+			Color:        "white",
+			LibertyCount: 4,
+		}
+
+		actual := isEnemyStone(mockEmptyStone.Color, mockWhiteStone.Color)
+		expected := false
+
+		if actual != expected {
+			t.Errorf("got %v and expected %v", actual, expected)
+		}
+	})
+}
+
+func TestIsFriendlyStone(t *testing.T) {
+
+	t.Run("A black stone and a white stone are not friends", func(t *testing.T) {
+
+		mockBlackStone := models.Stone{
+			Color:        "black",
+			LibertyCount: 4,
+		}
+
+		mockWhiteStone := models.Stone{
+			Color:        "white",
+			LibertyCount: 4,
+		}
+
+		actual := isFriendlyStone(mockBlackStone.Color, mockWhiteStone.Color)
+		expected := false
+
+		if actual != expected {
+			t.Errorf("got %v and expected %v", actual, expected)
+		}
+	})
+
+	t.Run("Two white stones are friends", func(t *testing.T) {
+
+		mockWhiteStone1 := models.Stone{
+			Color:        "white",
+			LibertyCount: 4,
+		}
+
+		mockWhiteStone2 := models.Stone{
+			Color:        "white",
+			LibertyCount: 4,
+		}
+
+		actual := isFriendlyStone(mockWhiteStone1.Color, mockWhiteStone2.Color)
+		expected := true
+
+		if actual != expected {
+			t.Errorf("got %v and expected %v", actual, expected)
+		}
+	})
+
+	t.Run("If one stone color is empty, there is no friend", func(t *testing.T) {
+
+		mockEmptyStone := models.Stone{
+			Color:        "empty",
+			LibertyCount: 4,
+		}
+
+		mockWhiteStone := models.Stone{
+			Color:        "white",
+			LibertyCount: 4,
+		}
+
+		actual := isFriendlyStone(mockEmptyStone.Color, mockWhiteStone.Color)
+		expected := false
+
+		if actual != expected {
+			t.Errorf("got %v and expected %v", actual, expected)
+		}
+	})
+}
